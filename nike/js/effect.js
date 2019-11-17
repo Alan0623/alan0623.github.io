375d794ec3500deb1c447987ea0827ae1a2cb117
Alan Lin <alan623@mail.hamastar.com.tw>
2019-11-17 13:18:48 +0800

    第五次推送

--- nike/js/effect.js
+++ nike/js/effect.js
@@ -1,8 +1,7 @@
+//RWD輪播================================================================
 $(document).ready(function() {
     console.log("Hello!! Nice to see you.(\u30fb\u2200\u30fb)."), 
     console.log("Front-end programming By Alan Lin"), 
-
-//RWD輪播================================================================
     $('.autoplay3').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -10,14 +9,14 @@ $(document).ready(function() {
         autoplaySpeed: 5000,
         responsive: [
             {
-              breakpoint: 600,
+              breakpoint: 750,
               settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
               }
             },
             {
-              breakpoint: 480,
+              breakpoint: 600,
               settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
@@ -41,14 +40,14 @@ $(document).ready(function() {
               }
             },
             {
-              breakpoint: 600,
+              breakpoint: 750,
               settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
               }
             },
             {
-              breakpoint: 480,
+              breakpoint: 500,
               settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
@@ -58,32 +57,39 @@ $(document).ready(function() {
     });
 
 });
-//RWD輪播================================================================
-function show_browse(){
-//alert('dw wd');
-jarallax = new Jarallax();
-//jarallax.setDefault('h2,p', { display: 'none' });
-jarallax.addAnimation('.react', [{ progress: "0%", top: "0%" }, { progress: "100%", top: "-100%" }]);
-jarallax.addAnimation('.air-max-270-react-0JdRkV',  [{ progress: "0%", top: "0%" }, { progress: "100%", top: "-80%" }]);
-jarallax.addAnimation('.nike_air_max', [{ progress: "0%", top: "0%" }, { progress: "100%", top: "-20%" }]);
-jarallax.addAnimation('.content_media_block_model', [{ progress: "0%", top: "300%" }, { progress: "100%", top: "-300%" }]); 
-/*
-jarallax.addAnimation('h1', [{ progress: "0", top: "50px", opacity: "1" }, { progress: "10", top: "0px", opacity: "0" }]);
-jarallax.addAnimation('h2', [{ progress: "10", left: "-100%", display: 'block' },
-                            { progress: "12", left: "50%", display: 'block'},
-                            { progress: "24", left: "55%", }]);
+//jarallax===============================================================
+  function show_browse(){
+  jarallax = new Jarallax();
+  jarallax.addAnimation('.react', [{ progress: "0%", top: "0%" }, { progress: "100%", top: "-100%" }]);
+  jarallax.addAnimation('.air-max-270-react-0JdRkV',  [{ progress: "0%", top: "0%" }, { progress: "100%", top: "-80%" }]);
+  jarallax.addAnimation('.nike_air_max', [{ progress: "0%", top: "0%" }, { progress: "100%", top: "-20%" }]);
+  }
 
+  $(document).ready(function () {
+      show_browse();
 
-jarallax.addAnimation('h2',[{ progress: "20", opacity: "1" },
-                            { progress: "24", opacity: "0" }]);
-jarallax.addAnimation('p', [{ progress: "23", opacity: "0", display: 'block'},
-                            { progress: "25", opacity: "1", display: 'block' }, 
-                            { progress: "27", opacity: "1", display: 'block' },
-                            { progress: "28", opacity: "0"}]);*/
+  });
+//jarallax===============================================================
 
-}
-
-$(document).ready(function () {
-    show_browse();
-
+jQuery(window).load(function () {
+    $("body").removeClass("no_js")
+  }),
+$(".open_nav").click(function () {
+  $(this).toggleClass("open"),
+  $(".head_menu_nav_main").slideToggle(500),
+  window.addEventListener("resize", function () {
+    $(window).width() > 768 && ($(".head_menu_nav_main").removeAttr("style"),
+      $(".open_nav").removeClass("open"))
+  })
+}),
+$(window).scroll(function () {
+  var a = $(this).scrollTop(),
+    b = 90;
+  $(".head").css(a > b ? {
+    background: "rgba(248, 245, 238, 0.9)",
+    border: "1px solid rgb(255, 255, 255)"
+  } : {
+    background: "none",
+    border: "none"
+  })
 });
