304ca5d9ff5bb65c3ea125612f3f61b2e9551715
Alan Lin <alan0623@icloud.com>
2019-12-11 18:14:09 +0800

    pad版

--- appedu/js/effect.js
+++ appedu/js/effect.js
@@ -1,61 +1,7 @@
-//RWD輪播================================================================
+================================================================
 $(document).ready(function() {
     console.log("Hello!! Nice to see you.(\u30fb\u2200\u30fb)."), 
     console.log("Front-end programming By Alan Lin"), 
-    $('.autoplay3').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        responsive: [
-            {
-              breakpoint: 750,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-        ]
-    });
-    $('.autoplay4').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-              }
-            },
-            {
-              breakpoint: 750,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 560,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-        ]
-    });
-
 });
 //jarallax===============================================================
   function show_browse(){
@@ -67,7 +13,6 @@ $(document).ready(function() {
 
   $(document).ready(function () {
       show_browse();
-
   });
 //jarallax===============================================================
 
