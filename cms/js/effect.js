30bd83d540f9f795f0125d0eda6abb8eceec7bef
Alan Lin <alan623@mail.hamastar.com.tw>
2020-11-07 16:23:27 +0800

    cms

---
+++ cms/js/effect.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    console.log("Hello!! Nice to see you.(\u30fb\u2200\u30fb)."), 
+    console.log("Front-end programming By Alan Lin");
+});
+
+
+//jarallax===============================================================
+$(function() {
+    $(".personal").hover(function(){
+        $(".personal_functions").toggle();
+    });
+});
